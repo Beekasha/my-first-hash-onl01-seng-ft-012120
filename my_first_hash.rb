c9871457b7e698c11791ac86b98fa4e89d40f074
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-01-23 22:37:18 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,47 +1,34 @@
-def my_hash(k, v)
+def my_hash()
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   your_hash = {"k" => "v" }
-  
 end
-
 
 def shipping_manifest()
- 
   the_manifest = {
-                    "whale bone corsets" => 5, 
-                    "porcelain vases" => 2, 
-                    "oil paintings" => 3
-}
-
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3 
+  }
 end
 
-def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-}
+def retrieval()
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3 
+  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-    shipping_manifest["oil paintings"]
-
+  return the_manifest["oil paintings"]
 end
 
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest["muskets"] = 2
+def adding()
+  the_manifest = {
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3 
+  }
 
-
-  # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powder"] = 4
-
-
-  # return the shipping_manifest hash below
-  shipping_manifest
-
+  the_manifest["muskets"] = 2
+  the_manifest["gun powder"] = 4
+  the_manifest
 end
